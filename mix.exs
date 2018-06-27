33a22ae5b5c19614f40a3d492eee47a2213ac55a
Bernardo Amorim <contato@bamorim.com>
2018-06-27 12:00:17 -0300

    chore: stream_data is only for test

--- mix.exs
+++ mix.exs
@@ -22,9 +22,9 @@ defmodule ExchemaJSONish.MixProject do
   defp deps do
     [
       {:exchema, "~> 0.3.0"},
-      {:exchema_coercion, github: "bamorim/exchema_coercion", only: [:test]},
-      {:exchema_stream_data, github: "bamorim/exchema_stream_data", only: [:test]},
-      {:stream_data, "~> 0.4.2"}
+      {:exchema_coercion, github: "bamorim/exchema_coercion", only: :test},
+      {:exchema_stream_data, github: "bamorim/exchema_stream_data", only: :test},
+      {:stream_data, "~> 0.4.2", only: :test}
     ]
   end
 end
